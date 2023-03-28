5e00b95cc2783b0744def3321b3e075ed15c3b32
Arthur <martinsarthur07@gmail.com>
2023-03-28 20:22:54 -0300

    feat: change the return of the get methods of the employee route

--- backend/src/service/employees.service.js
+++ backend/src/service/employees.service.js
@@ -1,29 +1,59 @@
 const prismaClient = require('../dataBase/prismaClient');
 
 async function getAllEmployees() {
-  const allEmployees = await prismaClient.employees.findMany();
+  const allEmployees = await prismaClient.employees.findMany({
+    select: {
+      id: true,
+      name: true,
+      salary: true,
+      birth: true,
+      department: {
+        select: {
+          department_name: true,
+        },
+      },
+    },
+  });
   return allEmployees;
 }
 
 async function getOneEmployees(emplyeeId) {
-  const employee = await prismaClient.employees.findFirst({ where: { id: emplyeeId } });
+  const employee = await prismaClient.employees.findFirst({
+    where: { id: emplyeeId },
+    select: {
+      id: true,
+      name: true,
+      salary: true,
+      birth: true,
+      department: {
+        select: {
+          department_name: true,
+        },
+      },
+    },
+  });
   return employee;
 }
 
 async function addNewEmployee(emplyeeData) {
-  const newEmployee = await prismaClient.employees.create({ data: { ...emplyeeData } });
+  const newEmployee = await prismaClient.employees.create({
+    data: { ...emplyeeData },
+  });
   return newEmployee;
 }
 
 async function updateEmployee(emplyeeId, emplyeeData) {
   const updatedEmployee = await prismaClient.employees.update({
-    where: { id: emplyeeId }, data: { ...emplyeeData },
+    where: { id: emplyeeId },
+    data: { ...emplyeeData },
   });
   return updatedEmployee;
 }
 
 async function deleteEmployee(emplyeeId) {
-  const deletedEmployee = await prismaClient.employees.delete({ where: { id: emplyeeId } });
+  const deletedEmployee = await prismaClient.employees.delete({
+    where: { id: emplyeeId },
+  });
   return deletedEmployee;
 }
 
