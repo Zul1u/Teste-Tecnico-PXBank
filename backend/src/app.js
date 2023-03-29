1fd10bb37038d4090b681123e4f82c9172a08218
Arthur <martinsarthur07@gmail.com>
2023-03-29 10:54:29 -0300

    feat: install cors and start using

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 require('express-async-errors');
+const cors = require('cors');
 const employeeRoute = require('./routes/employee.routes');
 const departmentRoute = require('./routes/department.routes');
 const errorMiddleware = require('./middleware/error.middleware');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/employee', employeeRoute);
