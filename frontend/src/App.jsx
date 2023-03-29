288a61b1079f991ae76c19b061d094004a261616
Arthur <martinsarthur07@gmail.com>
2023-03-29 13:57:00 -0300

    feat: add the route from employees screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import './App.css';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Employees from './pages/Employees';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Routes>
+      <Route path="/" element={<Navigate to="/employees" />} />
+      <Route path="/employees" element={<Employees />} />
+    </Routes>
   );
 }
 
